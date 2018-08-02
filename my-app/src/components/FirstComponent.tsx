35c5f49a226b4d45ced90476cd97725b4f64ab7f
bentong95923 <19847553+bentong95923@users.noreply.github.com>
2018-08-03 01:42:03 +1200

    Modified the first component as a tool to find more details on a country which user wants to look for. It still cannot receive the desired data from the api for getting the result back to the user at the moment.

--- my-app/src/components/FirstComponent.tsx
+++ my-app/src/components/FirstComponent.tsx
@@ -6,8 +6,65 @@ export default class FirstComponent extends React.Component<{}> {
                 return (
                         <div className="centreText">
                                 {/* React components must have a wrapper node/element */}
-                                <h1>(▀̿Ĺ̯▀̿ ̿)</h1>
+                                <div className="textareaFirst">
+                                        Finding Country details: 
+                                        <input type="text/plain" id="countryName" onKeyPress={this.handleOnChange} placeholder="Enter country name"
+                                        />
+                                </div>
                         </div>
                 );
         }
+
+        public handleOnChange = (event: any) => {
+                if (event.key === 'Enter' && event.target.value.length > 2) {
+                        this.searchCountry(event);
+                }
+        }
+
+        public searchCountry = (event: any) => {
+                alert('https://restcountries.eu/rest/v2/name/'+encodeURI(event.target.value));
+                /* Calling api */
+                /* https://restcountries.eu/rest/v2/name/ */
+                
+                fetch('https://restcountries.eu/rest/v2/name/'+encodeURI(event.target.value)+"?fullText=true", {
+                        method: 'GET'
+                })
+                .then((response : any) => { // => is short hand for function declaration
+                        if (!response.ok) {
+                          this.setState({results: response.statusText})
+                          alert(response);
+                        }
+                        else {
+                                // response[0] = response;
+                                alert(JSON.stringify(response));
+                                // alert(response[0].name);
+                          // response.json().then((data:any) => this.setState({results: data[0].class}))
+                        }
+                        return response
+                })
+
+        }
+        /*
+        public upload(base64String: string) {
+                fetch('https://danktrigger.azurewebsites.net/api/dank', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'text/plain',
+                  },
+                  body: JSON.stringify({
+                    file: base64String,
+                  })
+                })
+                .then((response : any) => { // => is short hand for function declaration
+                  if (!response.ok) {
+                    this.setState({results: response.statusText})
+                  }
+                  else {
+                    response.json().then((data:any) => this.setState({results: data[0].class}))
+                  }
+                  return response
+                })
+              }
+            }*/
+
 }
