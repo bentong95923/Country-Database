5773098ff02d8b2ea70ec7c000fc63819a87f88e
bentong95923 <19847553+bentong95923@users.noreply.github.com>
2018-08-03 12:24:46 +1200

    Rewrite fetch() for calling api and receive the data correctly from the server. Interface state has constructed for storing the result of all the countries received from the server.

--- my-app/src/components/FirstComponent.tsx
+++ my-app/src/components/FirstComponent.tsx
@@ -1,6 +1,16 @@
 import * as React from "react";
+interface ICountryDetails {
+        countryList : {}
+}
 
-export default class FirstComponent extends React.Component<{}> {
+export default class FirstComponent extends React.Component<{}, ICountryDetails> {
+
+        constructor(props: any) {
+                super(props)
+                this.state = {
+                        countryList : {}
+                }
+        }
 
         public render() {
                 return (
@@ -8,63 +18,37 @@ export default class FirstComponent extends React.Component<{}> {
                                 {/* React components must have a wrapper node/element */}
                                 <div className="textareaFirst">
                                         Finding Country details: 
-                                        <input type="text/plain" id="countryName" onKeyPress={this.handleOnChange} placeholder="Enter country name"
+                                        <input type="text/plain" id="countryName" onKeyDown={this.handleOnChange} placeholder="Enter country name"
                                         />
+                                </div>
+                                <div className="displayCountry">
+                                        {JSON.stringify(this.state.countryList)}
                                 </div>
                         </div>
                 );
         }
 
-        public handleOnChange = (event: any) => {
-                if (event.key === 'Enter' && event.target.value.length > 2) {
-                        this.searchCountry(event);
+        public handleOnChange = (event: any) => {                
+                if (event.target.value.length > 2) {
+                        this.searchCountry(event.target.value);
                 }
         }
 
-        public searchCountry = (event: any) => {
-                alert('https://restcountries.eu/rest/v2/name/'+encodeURI(event.target.value));
+        public searchCountry = (country: string) => {
                 /* Calling api */
-                /* https://restcountries.eu/rest/v2/name/ */
-                
-                fetch('https://restcountries.eu/rest/v2/name/'+encodeURI(event.target.value)+"?fullText=true", {
-                        method: 'GET'
-                })
-                .then((response : any) => { // => is short hand for function declaration
-                        if (!response.ok) {
-                          this.setState({results: response.statusText})
-                          alert(response);
+                const url = 'https://restcountries.eu/rest/v2/name/'+encodeURI(country);
+                fetch(url)
+                .then(response => response.json())
+                .then((out) => {
+                        if (out.status !== 404) {
+                                this.setState({countryList : out});
+                        } else {
+                                // 404 Not result found
+                                this.setState({countryList : out.message});
                         }
-                        else {
-                                // response[0] = response;
-                                alert(JSON.stringify(response));
-                                // alert(response[0].name);
-                          // response.json().then((data:any) => this.setState({results: data[0].class}))
-                        }
-                        return response
                 })
+                .catch(err => alert(err));
 
         }
-        /*
-        public upload(base64String: string) {
-                fetch('https://danktrigger.azurewebsites.net/api/dank', {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'text/plain',
-                  },
-                  body: JSON.stringify({
-                    file: base64String,
-                  })
-                })
-                .then((response : any) => { // => is short hand for function declaration
-                  if (!response.ok) {
-                    this.setState({results: response.statusText})
-                  }
-                  else {
-                    response.json().then((data:any) => this.setState({results: data[0].class}))
-                  }
-                  return response
-                })
-              }
-            }*/
 
 }
