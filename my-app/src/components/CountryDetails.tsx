2ca4a91ab1c36248959767c63ae9b90b2e90a111
bentong95923 <19847553+bentong95923@users.noreply.github.com>
2018-08-05 12:13:50 +1200

    Country details will be displayed after the user confirms the query input. Iframe is used to trigger the API to get the country details after passing the selected Country from context.

--- my-app/src/components/CountryDetails.tsx
+++ my-app/src/components/CountryDetails.tsx
@@ -4,23 +4,51 @@ import * as React from "react";
     contains a named export called CContext.
 */
 import {CContext} from "./FirstComponent";
+interface ICountryDetails {
+    countryDetailsList: any[]
+}
 
-export default class CountryDetails extends React.Component<{}> {
+export default class CountryDetails extends React.Component<{}, ICountryDetails> {
 
     constructor(props: any) {
         super(props);
+        this.state = {
+            countryDetailsList: []
+        }
     }
 
     public render() {
         return (
-            <div className="centreText">
+            <div className="centreText">                
+                {/* An iframe is created for only allowing to auto call the api in the searchCountryDetails() to get the country details. It is not rendered. */}
+                <CContext.Consumer>
+                    {state=> <iframe className="iframeTriggerOnly" id={state.selectedCountry3Code} onLoad={this.searchCountryDetails} />}
+                </CContext.Consumer>
                 {/* React components must have a wrapper node/element */}
                 <h2>Country Details:</h2>
-                <CContext.Consumer>
-                    {state => <input value={state.test} />}
-                </CContext.Consumer>
+                {JSON.stringify(this.state.countryDetailsList)}
+                
+
             </div>
         );
+    }
+
+    public searchCountryDetails = (event: any) => {
+        /* Calling api from REST Countries website */
+        const url = 'https://restcountries.eu/rest/v2/alpha/' + event.target.id;
+        fetch(url)
+            .then(response => response.json())
+            .then((out) => {
+                if (out.status !== 404) {
+                    this.setState({ countryDetailsList: out});
+                } else {
+                    // 404 Not result found error, but should not reach here
+                    this.setState({ countryDetailsList: out.message});
+                }
+            })
+            .catch(err => alert(err)
+        );
+
     }
 
 }
--- my-app/src/components/FirstComponent.tsx
+++ my-app/src/components/FirstComponent.tsx
@@ -3,66 +3,82 @@ import CountryDetails from "./CountryDetails";
 
 // Exporting CContext so other Components can import this context for its use.
 // Context with a component tag will render its content to the current component.
-export const CContext = React.createContext({test: "NA"});
+export const CContext = React.createContext({selectedCountry3Code: "None"});
 
-interface ICountryDetails {
-    countryList: any[],
-    test: string,
-    resultFound: boolean // Variable storage for knowing if results are found.
+interface ISearchCountry {
+    countryOptions: any[],
+    selectedCountry3Code: string,
+    resultFound: boolean // Flag indicates if results are found.
+    confirmedQuery: boolean // Flag indicates if user confirms the input for the query
 }
 
-export default class FirstComponent extends React.Component<{}, ICountryDetails> {
+export default class FirstComponent extends React.Component<{}, ISearchCountry> {
 
     constructor(props: any) {
         super(props);
         this.state = {
-            countryList: [],
-            test: "test",
-            resultFound: false
+            countryOptions: [],
+            selectedCountry3Code: "None",
+            resultFound: false,
+            confirmedQuery: false
         }
     }
 
     public render() {
-        return (
-            <div className="centreText">
-                <CContext.Provider value={this.state}>
-                    <CountryDetails />
-                </CContext.Provider>
-                {/* React components must have a wrapper node/element */}
-                <div className="textareaFirst">
-                    <h3>Finding Country details:</h3>
-                    <input type="text/plain" id="countryName" onKeyUp={this.handleOnKeyUp} placeholder="Enter country name"
-                    />
+        if (this.state.confirmedQuery) {
+            return (
+                <div>                
+                    <CContext.Provider value={this.state}>
+                        <CountryDetails />
+                    </CContext.Provider>
                 </div>
-                <div className="displayCountry">
-                    {this.renderCountryList(this.state.countryList, this.state.resultFound)}
+            );
+        } else {
+            return (
+                <div className="centreText">
+                    {/* React components must have a wrapper node/element */}
+                    <div className="textareaFirst">
+                        <h3>Finding Country details:</h3>
+                        <input type="text/plain" id="countryName" onKeyUp={this.handleOnKeyUp} placeholder="Enter country name" />
+                    </div>
+                    <div className="displayCountry">
+                        {this.renderCountryList(this.state.countryOptions, this.state.resultFound)}
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 
     public handleOnKeyUp = (event: any) => {
         // User are required to input at least 3 letters to display any results
         if (event.target.value.length >= 3) {
-            this.searchCountry(event.target.value);
+            this.SearchCountries(event.target.value);
         } else if (event.target.value.length > 0){
-            this.setState({ countryList: ["Keep typing..."], resultFound: false});
+            this.setState({ countryOptions: ["Keep typing..."], resultFound: false});
         } else {
-            this.setState({ countryList: [], resultFound: false});
+            this.setState({ countryOptions: [], resultFound: false});
         }
     }
 
-    public searchCountry = (country: string) => {
+    public SearchCountries = (country: string) => {
         /* Calling api from REST Countries website */
         const url = 'https://restcountries.eu/rest/v2/name/' + encodeURI(country);
         fetch(url)
             .then(response => response.json())
             .then((out) => {
                 if (out.status !== 404) {
-                    this.setState({ countryList: out,  resultFound: true });
+                    const output = new Array();
+                    // Retrieve the alpha 3 code (Primary key) and the country name from the result
+                    out.map((value: any) => {
+                        output.push({
+                            id: value.alpha3Code,
+                            name: value.name
+                        })
+                    });
+                    this.setState({ countryOptions: output,  resultFound: true });
                 } else {
                     // 404 Not result found error, store the received message
-                    this.setState({ countryList: out.message, resultFound: false });
+                    this.setState({ countryOptions: out.message, resultFound: false });
                 }
             })
             .catch(err => alert(err)
@@ -74,8 +90,9 @@ export default class FirstComponent extends React.Component<{}, ICountryDetails>
         // Display result if results are found
         if (resultFound) {
             const content = countryJSON.map((value: any) =>
-                <div key={value.alpha3Code}>
-                    <h4>{value.name}</h4>
+                // key field is a unique key required for map to display JSON
+                <div key={value.id}>
+                    <h4 id={value.id} className="countryOptions" onClick={this.handleOnClickCountry}>{value.name}</h4>
                 </div>
             );
             return content;
@@ -83,6 +100,16 @@ export default class FirstComponent extends React.Component<{}, ICountryDetails>
             // Display the no found message to user.
             return <h4>{countryJSON}</h4>;
         }
+    }
+
+    public handleOnClickCountry = (event: any) => {
+        // need to paste the full country name back to the input field
+        /* const temp = this.state.countryOptions.find((value: any) => {
+            return value.id === event.target.id;
+        });*/
+        this.setState({selectedCountry3Code: event.target.id, confirmedQuery: true});
+        // alert(event.target.id);
+        return;
     }
 
 
