99a0f41960b476a554fd8affc40f4391f25365c2
bentong95923 <bentong95923@gmail.com>
2020-03-17 00:00:28 +0800

    debugging env

--- .netlify/functions/GetGoogleAPIKey.js
+++ .netlify/functions/GetGoogleAPIKey.js
@@ -1,6 +1,6 @@
 const { API_KEY_GOOGLE } = process.env;
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
     console.log(API_KEY_GOOGLE)
   return {
     statusCode: 200,
