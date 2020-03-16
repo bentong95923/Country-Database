f0bd8d40239f819934b97b491713923436201737
bentong95923 <bentong95923@gmail.com>
2020-03-17 00:50:37 +0800

    debugging env

--- .netlify/functions/hello_world.js
+++ .netlify/functions/hello_world.js
@@ -1,4 +1,12 @@
 exports.handler = async (event, context) => {
+    // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const params = JSON.parse(event.body);
+  console.log(params);
+
     return {
       statusCode: 200,
       body: "Hello, World"
