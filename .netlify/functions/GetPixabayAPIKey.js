f60edf73efb93c0c2f8f3bb8e77b5e7ef6a7456f
bentong95923 <bentong95923@gmail.com>
2020-03-17 00:24:36 +0800

    debugging env

--- .netlify/functions/GetPixabayAPIKey.js
+++ .netlify/functions/GetPixabayAPIKey.js
@@ -1,9 +1,9 @@
 const { API_KEY_PIXABAY } = process.env;
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
     console.log(API_KEY_PIXABAY)
-  return {
-    statusCode: 200,
-    body: API_KEY_PIXABAY
-  };
+    callback(null, {
+        statusCode: 200,
+        body: API_KEY_PIXABAY
+    });
 };
