8b7d055f5669da9199de86e15e93d714ec575bcd
bentong95923 <bentong95923@gmail.com>
2020-10-19 00:23:22 +0800

    seo

--- country-database/src/AppSEO.ts
+++ country-database/src/AppSEO.ts
@@ -1,3 +1,3 @@
-export const APP_SEO_DESC = "Look up any countries in the world! Provide detailed country's description (by Wikipedia), country's location (by Google Map) and country's scenery (by Pixabay)!";
+export const APP_SEO_DESC = "Look up any countries in the world! Country Database provides detailed country's description (by Wikipedia), location (by Google Map) and awesome scenery gallery (by Pixabay)!";
 
 export { default as SEOImage } from './img/seo_image.png';
